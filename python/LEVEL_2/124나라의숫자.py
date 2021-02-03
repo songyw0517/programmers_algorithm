7b3b81d56f47122244a196739caac6ebffc06413
songyw0517 <songyw0517@naver.com>
2021-02-04 04:51:15 +0900

    Create 124나라의숫자.py

---
+++ python/LEVEL_2/124나라의숫자.py
@@ -0,0 +1,12 @@
+def solution(n):
+    answer = ''
+    temp = [1,2,4]
+
+    while n > 0:
+        n-=1
+        i = n%3
+        answer = str(temp[i]) + answer
+        n= int(n/3)
+    return str(answer)
+
+print(solution(6))
