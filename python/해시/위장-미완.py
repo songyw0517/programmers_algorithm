d62a39d32abc564b1aaa4048ddf8138c56f7fc73
songyw0517 <songyw0517@naver.com>
2021-02-06 03:49:43 +0900

    Create 위장-미완.py

---
+++ python/해시/위장-미완.py
@@ -0,0 +1,11 @@
+def solution(clothes):
+    answer = {}
+    dict_clothes = dict(clothes)
+    item = dict_clothes.keys()
+    parts = dict_clothes.values()
+    
+    
+
+    return answer
+
+print(solution([['yellow_hat', 'headgear'], ['blue_sunglasses', 'eyewear'], ['green_turban', 'headgear']]))
