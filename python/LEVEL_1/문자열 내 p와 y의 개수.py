828a0e7c15358c3e72ea73da4edce02573878823
songyw0517 <songyw0517@naver.com>
2021-02-08 03:05:52 +0900

    Create 문자열 내 p와 y의 개수.py

---
+++ python/LEVEL_1/문자열 내 p와 y의 개수.py
@@ -0,0 +1,15 @@
+def solution(s):
+    answer = True
+    p = y = 0
+
+    for i in s:
+        if i == 'p' or i =='P':
+            p+=1
+        elif i == 'y' or i == 'Y':
+            y+=1
+    if p == y:
+        return True
+    else:
+        return False
+
+print(solution("pPoooyY"))
