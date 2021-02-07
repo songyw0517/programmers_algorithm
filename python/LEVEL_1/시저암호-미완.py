798f154a29e26fd3f4249ad5c373a9b6ede70d51
songyw0517 <songyw0517@naver.com>
2021-02-08 03:06:15 +0900

    Create 시저암호-미완.py

---
+++ python/LEVEL_1/시저암호-미완.py
@@ -0,0 +1,12 @@
+def solution(s, n):
+    answer = ''
+    S = list(s)
+    for i in S:
+        if i>='a' and i<='z': #소문자인 경우
+            chr(i) += n
+        elif i>='A' and i<='Z': #대문자인 경우
+            i += n
+    print(S)
+    
+    return answer
+print(solution("AB", 1))
