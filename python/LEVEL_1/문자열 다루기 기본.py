c664aa940b820ce2c8f2741a6e5e7fe629205ddd
songyw0517 <songyw0517@naver.com>
2021-02-08 03:05:59 +0900

    Create 문자열 다루기 기본.py

---
+++ python/LEVEL_1/문자열 다루기 기본.py
@@ -0,0 +1,12 @@
+def solution(s):
+    if len(s)==4 or len(s)==6:
+        for i in s:
+            if i>='0' and i<='9':
+                continue
+            else:
+                return False
+    else:
+        return False
+    return True
+
+print(solution("1234"))
