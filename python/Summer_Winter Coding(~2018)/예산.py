abb2eca0995b9ef21e2557d41eef73216b6eb132
songyw0517 <songyw0517@naver.com>
2021-02-09 06:27:25 +0900

    Create 예산.py

---
+++ python/Summer_Winter Coding(~2018)/예산.py
@@ -0,0 +1,13 @@
+def solution(d, budget):
+    answer = 0
+    for i in d[:]:
+        Min = min(d)
+        budget -= Min
+        if budget>=0:
+            answer+=1
+            print(Min)
+            d.remove(Min)
+        else:
+            break
+    return answer
+print(solution([2,2,3,3], 10))
