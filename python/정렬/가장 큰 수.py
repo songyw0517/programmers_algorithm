c892d63965598783dec03163b79f0eacfaf73d7f
songyw0517 <songyw0517@naver.com>
2021-02-14 22:11:44 +0900

    Update 가장 큰 수.py

--- python/정렬/가장 큰 수.py
+++ python/정렬/가장 큰 수.py
@@ -1,31 +1,19 @@
 def solution(numbers):
-    answer = [[],[],[],[],[],[],[],[],[]]
-    for i in range(1, 10):
-        for n in numbers:
-            answer[int(str(n)[0])].append(n)
-        for i in answer:
-            if i!=[]:
-                i.sort()
-        answer.pop([])
-        break
-    
-
-    print(answer)
-print(solution([2,3,1,222,22,13,12]))
-
+    for i, value in enumerate(numbers):
+        for j in range(10,0,-1):
+            
+            print(j)
+print(solution([1,2,3,4,5]))
 
 '''
-2 3 1 222 22 13 12 -> 1 13 / 2 22 222 / 3
-
-시간 초과
-
 from itertools import permutations
 def solution(numbers):
     answer = permutations(numbers)
+    
     result = []
     for i in answer:
         result.append(int(''.join(map(str, list(i)))))
     
-    return str(max(result))
+    #return str(max(result))
 print(solution([6,10,2]))
 '''
