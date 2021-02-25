00d0a0550ec6cba772793add152c09dd56df29c8
songyw0517 <songyw0517@naver.com>
2021-02-25 12:32:55 +0900

    Update 가장 큰 정사각형 찾기.py

--- python/LEVEL_2/가장 큰 정사각형 찾기.py
+++ python/LEVEL_2/가장 큰 정사각형 찾기.py
@@ -1,44 +1,26 @@
-def is_square(board, width, t, k, row, col):
-    for i in range(t, t+width):
-        for j in range(k-width, k):       
-            if board[i][j]!=1:
-                return False
-    return True
-    
 def solution(board):
-    count_one = 0
     row = len(board)
     col = len(board[0])
-    for i in board:
-        count_one += sum(i)
-    if count_one<4:
+    answer=0
+    i=j=1
+    if(row <= 1 or col <= 1) :
         return 1
-    i = j = width = answer=0
-    while i<=row:     
-        if j<col:
-            if board[i][j]==1:
-                width+=1
-                j+=1
-                count_one-=1
-                continue
-            else: # 0을 만나면, check
-                if width:
-                    if i+width<=row: # 정사각형을 만족할 수 있는지 확인
-                        if is_square(board, width, i, j, row, col):
-                            if answer<width*width:
-                                answer = width*width
-                    width=0
-                j+=1
-        else: # j>col
-            if width:
-                if i+width<=row:
-                    if is_square(board, width, i, j, row, col):
-                        if answer<width*width:
-                            answer = width*width
-                    width=0
-            j=0
+    while i<row:
+        if board[i][j]:
+            up = board[i-1][j]
+            left = board[i][j-1]
+            upleft = board[i-1][j-1]
+            Min = min(up, left, upleft)
+            board[i][j] = Min +1
+            answer = max(answer, Min+1)
+        if j<col-1:
+            j+=1
+        else:
+            j=1
             i+=1
-    return answer
+            
+
+    return answer**2
 print(
     solution([
         [1,1,0,0,1,1,1,1],
