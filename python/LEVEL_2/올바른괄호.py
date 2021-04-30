d7bed9070ac170613081a46b4e05c4eca133fe72
Scof <32575460+songyw0517@users.noreply.github.com>
2021-04-30 18:23:09 +0900

    Update 올바른괄호.py

--- python/LEVEL_2/올바른괄호.py
+++ python/LEVEL_2/올바른괄호.py
@@ -1,24 +1,16 @@
 def solution(s):
     stack = []
-    top = 0
-    if s[0] == ')' or s[len(s)-1] == '(' or list(s).count(')') != list(s).count('(') or list(s).count('(') > int(len(s)/2)+1 or list(s).count(')') > int(len(s)/2)+1:
-        return False
     for i in s:
-        if i == ')': # 닫혀있는 것이 들어오면
-            if stack == []:
-                return False
-            else:
-                try:
-                    if stack[top] != '(':
-                        return False
-                    else:
-                        top+=1
-                except:
-                    return False
-        else:
-            stack.append(i)
-    # [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-    return True
+        if stack == [] and i == ')':
+            return False # 잘못됨
+        elif i == ')': # 스택에서 하나빼기
+            stack.pop()
+        else: # (가 들어오면, 어팬드
+            stack.append('(')
+    if stack==[]:
+        return True
+    else:
+        return False
 print(solution("(())"))
 '''
 test data
