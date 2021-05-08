26b590a4d53a26434eaf8e8ee59ed4c4e11492de
Scof <32575460+songyw0517@users.noreply.github.com>
2021-05-08 18:46:04 +0900

    Update 점프와 순간 이동.py

--- python/Summer_Winter Coding(~2018)/점프와 순간 이동.py
+++ python/Summer_Winter Coding(~2018)/점프와 순간 이동.py
@@ -1,19 +1,13 @@
 # DFS같다
 
 def solution(n):
-    ans = n # 건전지 사용량, 최대값, 바로 점프하는 경우
-    for i in range(1, n):
-        print('i = ', i)
-        distance = temp_ans = i # i칸 점프
-        while distance < n: # 순간이동
-            distance = distance*2
-        if distance > n:
-            distance/=2
-        print(distance)
-        temp_ans += n-distance
-        print("temp = ", temp_ans)
-        if temp_ans < ans:
-            ans=temp_ans
+    ans = 0
+    while n>0:
+        if n % 2 == 0:
+            n /= 2
+        else:
+            n-=1
+            ans+=1
     return int(ans)
-print(solution(6))
+print(solution(5000))
 
