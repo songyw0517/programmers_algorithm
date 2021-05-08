3f47f76a63765a451cab2a09bd71d11ef54e7082
Scof <32575460+songyw0517@users.noreply.github.com>
2021-05-08 18:46:08 +0900

    Update 타겟 넘버-미완.py

--- python/깊이,너비 우선탐색(DFS/BFS)/타겟 넘버-미완.py
+++ python/깊이,너비 우선탐색(DFS/BFS)/타겟 넘버-미완.py
@@ -1,8 +1,22 @@
+def dfs(array, numbers, target, size):
+    answer = 0 
+    if len(array) == size: 
+        if sum(array) == target: 
+            return 1
+        else: 
+            return 0
+    else:
+        A = numbers.pop(0)
+        for i in [1, -1]:
+            array.append(A * i)
+            answer += dfs(array, numbers, target, size)
+            array.pop()
+        numbers.append(A)
+        return answer 
+
 def solution(numbers, target):
     answer = 0
-    numbers.sort()
-    dupList = []
-    
+    answer += dfs([numbers[0]], numbers[1:], target, len(numbers))
+    answer += dfs([-numbers[0]], numbers[1:], target, len(numbers))
     return answer
-
 print(solution([1,2,3,4,5], 0))
